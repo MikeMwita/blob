a6deffc0ab31c8ddb0a0b198ba9e03b02b35f8a9
MikeMwita <mwitamike8@gmail.com>
2023-09-08 10:55:18 +0300

    setup CORS middleware

--- main.go
+++ main.go
@@ -2,38 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
-	"os"
 	"time"
 )
 
 func main() {
 	r := gin.Default()
 	r.Use(CORSMiddleware())
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	if err := r.Run(":" + port); err != nil {
-		log.Panicf("error: %s", err)
-	}
 
 	r.GET("/api", func(c *gin.Context) {
-		slackName := c.Query("slack_name")
-		if slackName == "" {
-			slackName = "Michael"
-		}
-
-		track := c.Query("track")
-		if track == "" {
-			track = "backend"
-		}
-
+		slackName := c.DefaultQuery("slack_name", "Michael")
+		track := c.DefaultQuery("track", "backend")
 		currentDay := time.Now().UTC().Format("Friday")
 		currentUTC := time.Now().UTC()
 		currentTime := currentUTC.Format("2006-01-02T15:04:05Z")
 		githubFileURL := "https://github.com/MikeMwita/blob/blob/main/main/main.go"
+
 		githubRepoURL := "https://github.com/MikeMwita/blob"
 
 		responseData := gin.H{
@@ -56,13 +40,15 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS,GET,PUT")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
 		c.Next()
+
 	}
+
 }
